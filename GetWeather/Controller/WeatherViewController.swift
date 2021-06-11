47fb0ced10878de70ed455c95f25d02664d0c94e
Prashuk Ajmera <prashuk.ajmera@gmail.com>
2021-06-10 20:06:55 -0500

    Initial Commit with dark mode feature

---
+++ GetWeather/Controller/WeatherViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GetWeather
+//
+//  Created by Prashuk Ajmera on 01/09/2021.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,12 @@
+#  GetWeather
+
+Sample iPhone app to get the weather of city and as well the current location.
+
+## Features
+
+* Dark-mode enabled app with vector assets.
+* Delegate pattern.
+* URLSession to network and make HTTP requests.
+* Grand Central Dispatch to fetch the main thread.
+* Core Location to get the current location from the phone GPS. 
+
